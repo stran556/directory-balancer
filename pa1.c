44e6fcd76e274ed7fda8b506d5553b0066043a16
Sean <36829049+stran556@users.noreply.github.com>
2022-06-08 18:41:20 -0500

    Add files via upload

--- pa1.c
+++ pa1.c
@@ -2,15 +2,49 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-	int id = fork();
-	printf("Weve got a job to do\n");
-
-	if (id == 0) {
-		printf("Child Process\n");
-	} else {
-	printf("Parent Process\n");
+	
+	int p1[2];
+	if (pipe(p1) == -1) {
+		return 1;
 	}
+	int pid = fork();
+	if (pid == -1) {
+		return 2;
+	}
+	if (pid == 0) {
+		//child
+		int x;
+		if (read(p1[0], &x, sizeof(x)) == -1) { //read data from parent process
+			return 3; 
+		}
+		printf("Received %d\n", x);
+		x *= 4;
+		if (write(p1[1], &x, sizeof(x)) == -1) { 
+			return 4; 
+		}
+		printf("Wrote %d\n", x);
+	} 
+	else {
+		//Parent
+		srand(time(NULL));
+		int y = rand() % 10;
+		if (write(p1[1], &y, sizeof(y)) == -1) {
+			return 5;
+		}
+		printf("Wrote %d\n", y);
+		if (read(p1[0], &y, sizeof(y)) == -1) {
+			return 6;
+		}
+		printf("Result is %d\n", y);
+		wait(NULL);
+	}
+	char* command;
+	strcpy(command, "ls");
+	system(command);
 	return 0;
 }
