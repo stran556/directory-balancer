355880b9ef308dd0b369c5fce4465918798900cd
Sean <36829049+stran556@users.noreply.github.com>
2022-06-08 21:29:32 -0500

    Add files via upload

--- pa1.c
+++ pa1.c
@@ -8,30 +8,67 @@
 //fd[0] = input, write
 //fd[1] = output, read
 int main(int argc, char* argv[]) {
-	
+
+	char txt[100];
+	//Verify initial discrepancies between d1 and d2
+	system("diff d1 d2");
+
 	//Clear both files
 	system(": > d1.txt");
 	system(": > d2.txt");
 
-	int p1[2]; //fd[0], fd[1] main -> child
-	int p2[2]; //fd[0], fd[1] child -> main
+	int p1[2]; //main -> child
+	int p2[2]; // child -> main
+
+	//Confirm successful pipe initialization
 	if (pipe(p1) == -1) {
+		printf("Error.");
+		return 1;
+	}
+	if (pipe(p2) == -1) {
+		printf("Error.");
 		return 1;
 	}
 	int pid = fork();
 	if (pid == -1) {
 		return 0;
 	}
-	if (pid == 0) {
-		//child
-		system("ls d2 >> d2.txt"); //child process to directory 2
-		system("cat d2.txt");
+
+	char command[100];
+	//child
+	if (pid == 0) { //child, so use pipe p2
+		char* directory = "d2"; //assign child process to directory 2
 		
-	} 
-	else {
-		//main
-		system("ls d1 >> d1.txt"); //main process to directory 1
-		system("cat d1.txt");
+		//text file name
+		
+		
+		//add file names to list
+		sprintf(command, "ls %s >> %s.txt", directory, directory);
+		system(command); 
+
+		//test print
+		sprintf(command, "cat %s.txt", directory);
+		system(command);
+	}
+
+	//main 
+	else { //main, so use pipe p1
+		char* directory = "d1"; //assign main process to directory 1
+		sprintf(txt, "%s.txt", directory);
+
+		//text file name
+		sprintf(txt, "%s.txt", directory);
+
+		//add file names to list
+		sprintf(command, "ls %s >> %s", directory, txt);
+		system(command); 
+
+		//test print
+		sprintf(command, "cat %s.txt", directory);
+		system(command);
+
+		//write(p1[1], str)
+		
 		//srand(time(NULL));
 		/*
 		int y = rand() % 10;
